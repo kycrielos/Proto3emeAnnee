4af5fb6a73eb74780134022554deb326bde95d5b
Kycrielos <loick.sery@gmail.com>
2020-11-04 15:08:15 +0100

    MonstreViewFollow

---
+++ Assets/Script/SnakeScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeScript : MonoBehaviour
+{
+    public Transform Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player != null)
+        {
+            transform.LookAt(new Vector3(Player.position.x, transform.position.y , Player.position.z));
+        }
+    }
+}
