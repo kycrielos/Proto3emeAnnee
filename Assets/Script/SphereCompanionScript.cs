68751e9db778e41fe7fcf1c6f954a9bc92377223
Kycrielos <loick.sery@gmail.com>
2020-11-09 15:20:09 +0100

    ChangementPourFairePlaisirAAxel

--- Assets/Script/SphereCompanionScript.cs
+++ Assets/Script/SphereCompanionScript.cs
@@ -7,15 +7,32 @@ public class SphereCompanionScript : MonoBehaviour
 {
     private NavMeshAgent Agent;
     public GameObject Player;
+
+    public bool FollowPlayer;
     // Start is called before the first frame update
     void Start()
     {
         Agent = GetComponent<NavMeshAgent>();
+        FollowPlayer = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Agent.SetDestination(Player.transform.position);
+        if (FollowPlayer)
+        {
+            Agent.SetDestination(Player.transform.position);
+        }
+        else
+        {
+            Agent.SetDestination(transform.position);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "CompanionExitTrigger")
+        {
+            FollowPlayer = false;
+        }
     }
 }
---
+++ Assets/Script/SphereCompanionTriggerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SphereCompanionTriggerScript : MonoBehaviour
+{
+    public SphereCompanionScript Companion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Companion.FollowPlayer = true;
+        }
+    }
+}
