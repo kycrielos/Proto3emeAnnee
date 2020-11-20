9da59c54c435fc0085bbb8309450a8f2e30d8d19
Axel-Bouquin <ignistenglede@gmail.com>
2020-11-20 11:40:40 +0100

    LD 2 20/11/2020

---
+++ Assets/Script/PlatformBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformBehaviour : MonoBehaviour
+{
+    public Transform pos1, pos2;
+    public float speed, offset;
+
+    private Transform targetPos;
+    private bool changeTarget;
+
+    void Start()
+    {
+        targetPos = pos1;
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(targetPos.position, transform.position);
+        if (distance <= offset)
+        {
+            changeTarget = !changeTarget;
+        }
+
+        if (changeTarget)
+        {
+            targetPos = pos2;
+        } else
+        {
+            targetPos = pos1;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPos.position, speed * Time.deltaTime);
+    }
+}
