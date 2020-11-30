e9604fc0aae91b00159001ee60790068b953d550
Kycrielos <loick.sery@gmail.com>
2020-11-30 13:02:33 +0100

    Ld_Update

---
+++ Assets/01_SCRIPTS/JetVapeurScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JetVapeurScript : MonoBehaviour
+{
+    public float TickPerSecond;
+    public float DamagePerTick;
+    private float TickTimer;
+
+    private bool Active;
+
+    private float ActiveTimer;
+    public float ActiveDelay;
+
+    private void Update()
+    {
+        ActiveTimer += Time.deltaTime;
+        if (ActiveTimer >= ActiveDelay && !Active)
+        {
+            ActiveTimer = 0;
+            Active = true;
+        }
+        else if (ActiveTimer >= ActiveDelay && Active)
+        {
+            ActiveTimer = 0;
+            Active = false;
+        }
+
+        if (Active)
+        {
+            GetComponent<Renderer>().enabled = true;
+        }
+        else
+        {
+            GetComponent<Renderer>().enabled = false;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && Active)
+        {
+            TickTimer += Time.deltaTime;
+            if (TickTimer >= 1 / TickPerSecond)
+            {
+                other.GetComponent<PlayerController>().HP -= DamagePerTick;
+                TickTimer = 0;
+            }
+        }
+    }
+}
