eb2cff26ab047a99a00df5aac3e435f7ae63b6aa
Kycrielos <loick.sery@gmail.com>
2021-01-11 14:30:43 +0100

    Spike

---
+++ Assets/01_SCRIPTS/SpikesScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikesScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<PlayerController>().HP = 0;
+        }
+    }
+}
