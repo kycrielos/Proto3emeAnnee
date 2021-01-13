b6202cbed862a750bf39076e23a0b2cb923716dc
Axel-Bouquin <ignistenglede@gmail.com>
2021-01-13 11:07:43 +0100

    Integration du Sons d'Ambiance

---
+++ Assets/01_SCRIPTS/AmbiantSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbiantSound : MonoBehaviour
+{
+    public AudioSource SFX_AmbiantSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SFX_AmbiantSound.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
